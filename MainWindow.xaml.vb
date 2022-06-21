bab2cb5d4618ee65e23de5f54c565712c16c95ac
Trip <chrisbitschi@hotmail.com>
2022-06-21 14:24:23 +0300

    Add files via upload
    Completed Project

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Class MainWindow
+    Private Sub b_find_Click(sender As Object, e As RoutedEventArgs) Handles b_find.Click
+        Dim logi As Automation.AutomationElement
+        logi.FindFirst(Automation.TreeScope.Subtree, New Automation.Condition())
+        Dim aeh As Automation.AutomationEventHandler = New Automation.AutomationEventHandler(AddressOf EventHandler)
+    End Sub
+
+    Private Sub EventHandler(obj As Object, args As Automation.AutomationEventArgs)
+        Dim a As Integer = 1
+    End Sub
+End Class
